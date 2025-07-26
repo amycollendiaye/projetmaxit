e8f8a99a5abbb8b309c4e766c21cca7b9a7cf91a
amycollendiaye <amycollendiaye103@gmail.com>
2025-07-26 20:00:52 +0000

    mettant en commataires les define de mysql

--- app/config/env.php
+++ app/config/env.php
@@ -13,16 +13,16 @@ define('DB_DRIVER', $_ENV['DB_DRIVER']);
 define('DB_NAME', $_ENV['DB_NAME']);
  // LES  DNS DE MYSQL ET POSTGRES
 $DB_DSN_POSTGRES="{$_ENV['DB_DRIVER']}:host={$_ENV['DB_HOST_POSTGRES']};port={$_ENV['DB_PORT_POSTGRES']};dbname={$_ENV['DB_NAME']}";
-$DSN_MYSQL="{$_ENV['DB_DRIVER']}:host={$_ENV['DB_HOST_MYSQL']};port={$_ENV['DB_PORT_MYSQL']};dbname={$_ENV['DB_NAME']}";
+// $DSN_MYSQL="{$_ENV['DB_DRIVER']}:host={$_ENV['DB_HOST_MYSQL']};port={$_ENV['DB_PORT_MYSQL']};dbname={$_ENV['DB_NAME']}";
 define('DB_DSN_POSTGRES',$DB_DSN_POSTGRES);
-define('DB_DSN_MYSQL', $DSN_MYSQL);
+// define('DB_DSN_MYSQL', $DSN_MYSQL);
 
 
  /// LES NOM DES  BASE DE DNS ;
-$BASE_DSN_MYSQL="{$_ENV['DB_DRIVER']}:host={$_ENV['DB_HOST_MYSQL']};port={$_ENV['DB_PORT_MYSQL']}";
-$BASE_DSN_POSTGRES="{$_ENV['DB_DRIVER']}:host={$_ENV['DB_HOST_POSTGRES']};port={$_ENV['DB_PORT_POSTGRES']}";
- define('BASE_DSN_POSTGRES', $BASE_DSN_POSTGRES);
- define('BASE_DSN_MYSQL', $BASE_DSN_MYSQL);
+// $BASE_DSN_MYSQL="{$_ENV['DB_DRIVER']}:host={$_ENV['DB_HOST_MYSQL']};port={$_ENV['DB_PORT_MYSQL']}";
+// $BASE_DSN_POSTGRES="{$_ENV['DB_DRIVER']}:host={$_ENV['DB_HOST_POSTGRES']};port={$_ENV['DB_PORT_POSTGRES']}";
+//  define('BASE_DSN_POSTGRES', $BASE_DSN_POSTGRES);
+//  define('BASE_DSN_MYSQL', $BASE_DSN_MYSQL);
   
 
 
